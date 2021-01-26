0a22e6afd9e8a6d772493820e196b2dcd193c27e
miroxa81 <miroxa81@hotmail.com>
2021-01-26 23:49:20 +0200

    Work on Lesson3

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random Digits = new Random();
+			int DiagonalSelection;
+			int DiagonalOffset;
+
+			Console.WriteLine("Please enter your name:");
+			string name = Console.ReadLine();
+
+			Console.WriteLine($"Hello, {name}, please enter two dimensions of array:");
+
+
+			Console.WriteLine("Width:");
+			int ArrayWidth = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Height:");
+			int ArrayHigth = Convert.ToInt32(Console.ReadLine());
+
+
+			do
+			{
+				Console.WriteLine("Which diagonal to show? \n 1. main diagonal. \n 2. Side diagonal");
+				DiagonalSelection = Convert.ToInt32(Console.ReadLine());
+
+				if (DiagonalSelection == 1 || DiagonalSelection == 2)
+				{
+					break;
+				}
+			} while (true);
+
+			do
+			{
+				Console.WriteLine($"Which offset of diagonal ? From {ArrayWidth * (-1) + 1} to  {ArrayWidth - 1}");
+				DiagonalOffset = Convert.ToInt32(Console.ReadLine());
+
+				if (DiagonalOffset > ArrayWidth * (-1) && DiagonalOffset < ArrayWidth)
+				{
+					break;
+				}
+			} while (true);
+
+			int[,] Array = new int[ArrayWidth, ArrayHigth];
+
+			for (int i = 0; i < ArrayWidth; i++)
+			{
+				for (int j = 0; j < ArrayHigth; j++)
+				{
+					Array[i, j] = Digits.Next(0, 99);
+				}
+			}
+			for (int i = 0; i < ArrayWidth; i++)
+			{
+				for (int j = 0; j < ArrayHigth; j++)
+				{
+					if (DiagonalSelection == 1)
+					{
+
+						if (i + DiagonalOffset == j)
+						{
+							Console.ForegroundColor = ConsoleColor.Green;
+							if (Convert.ToString(Array[i, j]).Length == 1)
+							{
+								Console.Write($" {Array[i, j]} ");
+							}
+							else
+							{
+								Console.Write($"{Array[i, j]} ");
+							}
+
+							if (j == ArrayHigth - 1)
+							{
+								Console.WriteLine("\n");
+							}
+							Console.ResetColor();
+							continue;
+						}
+					}
+					if (DiagonalSelection == 2)
+					{
+						if (i + DiagonalOffset == ArrayHigth - j - 1)
+						{
+							Console.ForegroundColor = ConsoleColor.Green;
+							if (Convert.ToString(Array[i, j]).Length == 1)
+							{
+								Console.Write($" {Array[i, j]} ");
+							}
+							else
+							{
+								Console.Write($"{Array[i, j]} ");
+							}
+							if (j == ArrayHigth - 1)
+							{
+								Console.WriteLine("\n");
+							}
+							Console.ResetColor();
+							continue;
+						}
+					}
+					if (Convert.ToString(Array[i, j]).Length == 1)
+					{
+						Console.Write($" {Array[i, j]} ");
+					}
+					else
+					{
+						Console.Write($"{Array[i, j]} ");
+					}
+
+					if (j == ArrayHigth - 1)
+					{
+						Console.WriteLine("\n");
+					}
+				}
+			}
+			Console.ResetColor();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[,] Notebook = new string [5,2];
+
+
+		}
+	}
+}
