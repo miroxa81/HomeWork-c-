46dcddb3b8907fb2a68ca56cda72dc78c63fb9a2
miroxa81 <miroxa81@hotmail.com>
2021-01-28 20:36:05 +0200

    Task 1 OK

--- Lesson4/Task1/Program.cs
+++ Lesson4/Task1/Program.cs
@@ -6,21 +6,73 @@ namespace Task1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Please input your first name");
-			string Patronymic =  Convert.ToString(Console.ReadLine());
-			Console.WriteLine("Please input your last name");
-			string LastName = Convert.ToString(Console.ReadLine());
-			Console.WriteLine("Please input your patronymic");
-			string Patronymic = Convert.ToString(Console.ReadLine());
-
-			GetFullName(FirstName, LastName, Patronymic);
-		}
+			int count;
+			Console.WriteLine("Please input quantity users");
 			
+			count = ReadInt();
+			string[] Data = new string[count];
 
-		static string GetFullName(string firstName, string lastName, string patronymic)
+			for (int i = 0; i < count; i++)
+			{
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+				Console.WriteLine($"User #{i + 1}");
+				Console.ResetColor();
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				Console.WriteLine($"Input first name");
+				Console.ResetColor();
+				string FirstName = Convert.ToString(Console.ReadLine());
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				Console.WriteLine($"Input last name");
+				Console.ResetColor();
+				string LastName = Convert.ToString(Console.ReadLine());
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				Console.WriteLine($"Input patronymic");
+				Console.ResetColor();
+				string Patronymic = Convert.ToString(Console.ReadLine());
+
+				Data[i] = GetFullName(FirstName, LastName, Patronymic);
+			}
+
+/*			for (int i = 0; i < count; i++)
+			{
+				Console.WriteLine($"User #{i + 1}. {Data[i]}");
+			}
+			Console.ReadKey();
+*/
+			OutData(Data);
+		}
+
+
+		static int ReadInt()
 		{
-			string FullName = ($"{}{}{}");
-			
+			int number;
+			while (!int.TryParse(Console.ReadLine(), out number))
+			{
+				Console.ForegroundColor = ConsoleColor.DarkRed;
+				Console.WriteLine("Please input numerical data");
+				Console.ResetColor();
+			}
+			return number;
+
 		}
+
+
+		static string GetFullName(string FirstName, string LastName, string Patronymic)
+		{
+			string FullName = ($"{FirstName} {LastName} {Patronymic}");
+			return FullName;
+		}
+
+
+		static void OutData(string[] Data)
+				{
+					for (int i = 0; i<Data.GetLength(0); i++)
+						{
+						Console.WriteLine($"User #{i+1}. {Data[i]}");
+						}
+						Console.ReadKey();
+					return;
+				}
+		
 	}
 }
