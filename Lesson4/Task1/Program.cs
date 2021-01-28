aaedcce6eb48793f7ae30622e80efc4aa7ffa26e
miroxa81 <miroxa81@hotmail.com>
2021-01-28 19:33:49 +0200

    Lesson 4 Start

--- Lesson4/Task1/Program.cs
+++ Lesson4/Task1/Program.cs
@@ -6,7 +6,21 @@ namespace Task1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Please input your first name");
+			string Patronymic =  Convert.ToString(Console.ReadLine());
+			Console.WriteLine("Please input your last name");
+			string LastName = Convert.ToString(Console.ReadLine());
+			Console.WriteLine("Please input your patronymic");
+			string Patronymic = Convert.ToString(Console.ReadLine());
+
+			GetFullName(FirstName, LastName, Patronymic);
+		}
+			
+
+		static string GetFullName(string firstName, string lastName, string patronymic)
+		{
+			string FullName = ($"{}{}{}");
+			
 		}
 	}
 }
