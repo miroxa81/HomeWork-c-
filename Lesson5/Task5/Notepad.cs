ad9a603776bc3fae988e360a18a3c50eb1129046
Miroslav <miroxa81@hotmail.com>
2021-02-02 11:16:09 +0200

    task5 in process

---
+++ Lesson5/Task5/Notepad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5
+{
+	public class Notepad
+	{
+		public string Title { get; set; }
+		public bool Status { get; set; }
+		public DateTime ChangeDate { get; set; }
+
+
+		public Notepad(string title, bool status, DateTime changedate)
+		{
+
+			Title = title;
+			Status = status;
+			ChangeDate = changedate;		
+		}
+
+
+	}
+}
---
+++ Lesson5/Task5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Notepad[] ToDo = new Notepad();
+
+
+
+
+		}
+
+		static void Init()
+		{
+		}
+
+		static void Menu()
+		{
+			ConsoleKeyInfo kp;
+			bool isRepeat;
+			do
+			{
+				{
+					kp = Console.ReadKey();
+					if (kp.KeyChar == Convert.ToChar("y") || kp.KeyChar == Convert.ToChar("Y"))
+					{
+						isRepeat = true;
+					}
+					else
+					{
+						return;
+					}
+				}
+			} while (isRepeat);
+		}
+
+		static void WriteColor(string txt, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.Write(txt);
+			Console.ResetColor();
+		}
+	}
+
+
+}
